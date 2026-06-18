c2be281111cc83ce4c5ff4b3f43f103a96ca72f2
Saurav s Mehta <sauravmehta631@gmail.com>
2026-06-18 19:32:18 +0530

    git ignore file

--- frontend/src/components/common/Container.jsx
+++
@@ -1,5 +0,0 @@
-function Container({ children, className = '' }) {
-  return <div className={`section-shell ${className}`}>{children}</div>;
-}
-
-export default Container;
--- frontend/src/components/ui/Badge.jsx
+++
@@ -1,5 +0,0 @@
-function Badge({ children, className = '' }) {
-  return <span className={className}>{children}</span>;
-}
-
-export default Badge;
